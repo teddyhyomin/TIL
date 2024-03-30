be1ad07b33dd8e69cbb61cfabed34ae7d1b441fc
teddyhyomin <teddyhyominkim@gmail.com>
2024-03-30 16:55:16 -0400

    todo list add function

--- React/mini-project/App.js
+++ React/mini-project/App.js
@@ -20,9 +20,23 @@ const App = () => {
           checked: false,
         },
       ]);
-    return (
-        <TodoTemplate>
-            <TodoInsert/>
+      const nextId = useRef(4);
+
+      const onInsert = useCallback(
+        text => {
+          const todo = {
+            id: nextId.current,
+            text,
+            checked: false
+          };
+          setTodos(todos.concat(todo));
+          nextId.current += 1;
+        },
+        [todos],
+      );
+      return ( 
+        <TodoTemplate> 
+          <TodoInsert onInsert={onInsert}/>
             <TodoList todos={todos}/>
         </TodoTemplate>
     );
