1629c3542680b4dc59f120ea82ddfe9c88a353ec
teddyhyomin <teddyhyominkim@gmail.com>
2024-04-06 14:57:52 -0400

    toggle function add

--- React/mini-project/App.js
+++ React/mini-project/App.js
@@ -42,6 +42,17 @@ const App = () => {
         [todos],
       );
 
+      const onToggle = useCallback(
+        id => {
+          setTodos(
+            todos.map(todo =>
+              todo.id === id ? {...todo, checked: !todo.checked } : todo,
+              ),
+          );
+        },
+        [todos]
+      );
+      
       return ( 
         <TodoTemplate> 
           <TodoInsert onInsert={onInsert}/>
