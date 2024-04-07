fb7b288e0ed53a2614b3fdbf74b915e799cb02a2
teddyhyomin <teddyhyominkim@gmail.com>
2024-04-07 16:32:13 -0400

    react-virtualized randering

--- React/mini-project/TodoList.js
+++ React/mini-project/TodoList.js
@@ -1,21 +1,51 @@
+import React, { useCallback } from "react";
+//import { List } from "react-vitualized";
+import { List } from "../../node_modules/react-virtualized/dist/commonjs/index";
 import TodoListItem from "./TodoListItem";
 import './TodoList.scss';
 
-const TodoList = ({todos, onRemove, onToggle}) => {
-    return (...);
+const TodoList = ({ todos, onRemove, onToggle }) => {
     /*
-        <div className="TodoList">
+    return (...);
+      <div className="TodoList">
             {todos.map(todo => (
                 <TodoListItem 
                     todo={todo} 
                     key={todo.id} 
                     onRemove={onRemove}
-                    onToggle={onToggle} 
-                    />
+                    onToggle={onToggle}
+                />
             ))}
         </div>
     );
-    */
+*/
+    const rowRenderer = useCallback(
+        ({ index, key, style }) => {
+            const todo = todos[index];
+            return(
+                <TodoListItem
+                    todo={todo}
+                    key={key}
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                    style={style}
+                />
+            );
+        },
+        [onRemove, onToggle, todos],
+    );
+    return(
+        <List
+            className="TodoList"
+            width={512}
+            height={513}
+            rowCount={todos.length}
+            rowHeight={57}
+            rowRenderer={rowRenderer}
+            list={todos}
+            style={{ outline: 'none'}}
+         />   
+    );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
