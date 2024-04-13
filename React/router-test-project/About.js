d260246a1115ca928873b97a496aded47ac5bef8
teddyhyomin <teddyhyominkim@gmail.com>
2024-04-13 12:36:45 -0400

    How to use QueryString

--- React/router-test-project/About.js
+++ React/router-test-project/About.js
@@ -1,8 +1,13 @@
+import { useLocation } from "react-router-dom";
+
 const About = () => {
+    const location = useLocation();
+
     return (
         <div>
             <h1>Introduction</h1>
             <p>This projext is to use the REACT ROUTER.</p>
+            <p>Query String: {location.search} </p>
         </div>
     );
 };
