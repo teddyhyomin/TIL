d5ce5f8143850fcbb2e28827bfc058aa03739a94
teddyhyomin <teddyhyominkim@gmail.com>
2024-04-13 12:13:43 -0400

    add URL parameter

--- React/router-test-project/App.js
+++ React/router-test-project/App.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profile from "./pages/Profile";
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/profiles/:username" element={<Profile />} />
     </Routes>
   );
 };
