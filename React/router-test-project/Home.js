697fa0c237cfb8c7b4c546a2229aaa1615f370d6
teddyhyomin <teddyhyominkim@gmail.com>
2024-04-13 12:26:30 -0400

    URL Prameter fin

--- React/router-test-project/Home.js
+++ React/router-test-project/Home.js
@@ -5,7 +5,20 @@ const Home = () => {
         <div>
             <h1>HOME</h1>
             <p>This is the first page displayed.</p>
-            <Link to="/about"> INTRODUCTION </Link>
+            <ul>
+                <li>
+                    <Link to="/about"> INTRODUCTION </Link>
+                </li>
+                <li>
+                    <Link to="/profiles/neomuhwa"> neomuhwa's PROFILE </Link>
+                </li>
+                <li>
+                    <Link to="/profiles/theodor"> theodor's PROFILE </Link>
+                </li>
+                <li>
+                    <Link to="/profiles/void"> NOT EXISTED PROFILE </Link>
+                </li>
+            </ul>
         </div>
     );
 };
