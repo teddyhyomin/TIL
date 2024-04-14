4e03a2db6b54e91cff7692de5d67e843142454cd
teddyhyomin <teddyhyominkim@gmail.com>
2024-04-14 14:29:03 -0400

    index props

--- React/router-test-project/App.js
+++ React/router-test-project/App.js
@@ -1,16 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import About from "./About";
-import Article from "./Article";
-import Articles from "./Articles";
-import Home from "./Home";
-import Profile from "./Profile";
+import Layout from "./Layout";
+import About from "./pages/About";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+ {/*       <Route path="/" element={<Home />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+
       {/*}
       <Route path="/articles" element={<Articles />} />
       <Route path="/articles/:id" element={<Article />} />
