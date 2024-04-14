aca859a76ce6f06d994a661f697cd7260b341d73
teddyhyomin <teddyhyominkim@gmail.com>
2024-04-14 15:07:57 -0400

    NavLink example

--- React/router-test-project/Articles.js
+++ React/router-test-project/Articles.js
@@ -1,21 +1,71 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const Articles = () => {
+/*    const activeStyle = {
+        color: 'green',
+        fontSize: 21,
+    };
+
     return (
     <div>
         <Outlet />
         <ul>
             <li>
-                <Link to="/articles/1">ARTICLE 1</Link>
+                <NavLink
+                to="/articles/1"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                >
+                    ARTICLE 1
+                </NavLink>
             </li>
             <li>
-                <Link to="/articles/2">ARTICLE 2</Link>
+            <NavLink
+                to="/articles/2"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                >
+                    ARTICLE 2
+                </NavLink>
             </li>
             <li>
-                <Link to="/articles/3">ARTICLE 3</Link>
+            <NavLink
+                to="/articles/3"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                >
+                    ARTICLE 3
+                </NavLink>
             </li>
         </ul>
     </div>
+    )
+}
+*/
+
+    return (
+        <div>
+            <Outlet />
+            <ul>
+                <ArticleItem id = {1} />
+                <ArticleItem id = {2} />
+                <ArticleItem id = {3} />
+            </ul>
+        </div>
+    );
+};
+
+const ArticleItem = ({ id }) => {
+    const activeStyle = {
+        color: 'green',
+        fontSize: 21,
+    };
+    return (
+        <li>
+            <NavLink
+            to={`/articles/${id}`}
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+                ARTICLE {id}
+            </NavLink>
+        </li>
     )
 }
 
