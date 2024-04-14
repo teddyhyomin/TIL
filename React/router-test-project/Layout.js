c16b96e6137e633ae9764661514c30126865f798
teddyhyomin <teddyhyominkim@gmail.com>
2024-04-14 14:42:32 -0400

    How to use useNavigate

--- React/router-test-project/Layout.js
+++ React/router-test-project/Layout.js
@@ -1,10 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1);
+    }
+
+    const goArticles = () => {
+        navigate('/articles', { replace: true });
+    };
+        
     return (
         <div>
             <header style={{background: 'lightgray', padding: 16, fontsize: 24}}>
                 HEADER
+                <button onClick={goBack}> GO BACK </button>
+                <button onClick={goArticles}> Aricle LIST </button>  
             </header>
             <main>
                 <Outlet />
