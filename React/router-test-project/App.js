05029db445f8e1dff7d9a89d62e0c94780cfc8b6
teddyhyomin <teddyhyominkim@gmail.com>
2024-04-14 15:41:08 -0400

    Navigate component

--- React/router-test-project/App.js
+++ React/router-test-project/App.js
@@ -4,12 +4,15 @@ import About from "./pages/About";
 import Article from "./pages/Article";
 import Articles from "./pages/Articles";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
+import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <Routes>
-      <Route element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
  {/*       <Route path="/" element={<Home />} /> */}
         <Route path="/about" element={<About />} />
@@ -23,6 +26,9 @@ const App = () => {
       <Route path="/articles" element={<Articles />} >
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
---
+++ React/router-test-project/MyPage.js
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+
+const MyPage = () => {
+    const isLoggedIn = false;
+    
+    if(!isLoggedIn) {
+        return <Navigate to="/login" replace={true} />;
+    }
+
+    return <div> MY PAGE </div>;
+};
+
+export default MyPage;
